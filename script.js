5f22bdbd812a0a3699ddaac1cbc58a9a539b3232
Clayton Kenney <clayton.kenney@gmail.com>
2019-09-21 15:52:09 -0600

    final tweaks

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
     //Select random image from folder, and then change html to display image
-
     function cute () {
       let left = "";
       let right= "";
@@ -11,6 +10,7 @@
     }
     left = getRandom(1,13);
     right = getRandom(1,13);
+    //ensure left and right are different
     if (left==right) {
       right = getRandom(1,13);
       };
